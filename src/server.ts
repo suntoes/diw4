4a9cd2ad0f7cd6790c83c244c225ef05aae5a09a
suntoes <mar26xd@gmail.com>
2022-04-09 05:49:07 +0800

    port fix

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import { getData } from './utils/db'
 const app = express();
 
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const root = require('path').join(__dirname, '../client' ,'build')
 app.use(express.static(root));
